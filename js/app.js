62f7212f542b1126019530f9934d4615e17a6d54
Katie Zhou <katie.f.zhou@gmail.com>
2016-03-02 19:15:20 -0500

    changing colors again, background picture, portfolio spacing

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	console.log("Hi! If you're reading this, you're probably trying to take a peek at my website code. That's awesome. Why not let me help build your websites and applications? I am currently looking for a developer position, so if you have any openings, please let me know! My contact info is listed on the bottom of the page. Thanks!")
+	console.log("Hi! If you're reading this, you're probably taking a peek at my website code. That's awesome. I do the same thing. Why not let me help build your websites and applications? I am currently looking for a developer position, so if you have any openings, please let me know! My contact info is listed on the bottom of the page. Thanks!")
 
 	$(".parallax").parallax();
 
@@ -28,13 +28,29 @@ $(document).ready(function(){
   }, 1500);
 
   $(window).scroll(function(){
-		if ($(window).scrollTop() > 550) {
-			$("nav").removeClass("noscroll_nav");
-			$("nav").addClass("default_color");
+		if ($(window).scrollTop() > 500) {
+			$("nav").removeClass("noscroll-nav");
+			$("navnav").addClass("scroll-nav");
 		} else {
-			$("nav").addClass("noscroll_nav");
+			$("nav").addClass("noscroll-nav");
+			$("navnav").removeClass("scroll-nav");
 		};
 
 	});
 
+  
+  $('.tooltipped').tooltip({delay: 50});
+
+	$('.scrollspy').scrollSpy();
+ 
+	$(".button-collapse").sideNav();
+
+	$(".jstext").typed({
+    strings: ["Katie Zhou", "a developer", "a musician", "a thinker", "a creator"],
+    contentType: "html",
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 800,
+    loop: true
+  });
 });
